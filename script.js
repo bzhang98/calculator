f14e725acb055dda4c5dfabd92290c4591d2425f
Brian <brianzhang234@gmail.com>
2023-03-12 13:15:08 -0500

    rework to only evaluate one expression at a time

--- script.js
+++ script.js
@@ -1,130 +1,105 @@
 const eval = {
-    'add': (a, b) => a + b,
-    'subtract': (a, b) => a - b,
-    'multiply': (a, b) => a * b,
-    'divide': (a, b) => a / b,
+    'add': (a, b) => Number(a) + Number(b),
+    'subtract': (a, b) => Number(a) - Number(b),
+    'multiply': (a, b) => Number(a) * Number(b),
+    'divide': (a, b) => Number(a) / Number(b),
 }
 
-//Takes an expression (e.g. "1 + 2 * 3") and splits the string into an array by the space character yielding [1, '+', 2, '*', 3]
-//While the array contains an operator (+, -, ÷, or x), evaluates the expression of that portion of the array
-//and returns a new array with the result of that expression
-//(e.g. [1, '+', 2, '*', 3] will first evaluate 2 * 3 and return [1, '+', 6] then evaluate 1 + 6 and return [7]
-function evaluate(expression) {
-    let parts = expression.split(' ').map(part => !isNaN(part) ? Number(part) : part);
-    while (parts.includes('x')) {
-        let multiplyLocation = parts.indexOf('x');
-        let evalMultiply = eval.multiply(parts[multiplyLocation - 1], parts[multiplyLocation + 1]);
-        parts.splice(multiplyLocation - 1, 3, evalMultiply);
-    }
-    while (parts.includes('÷')) {
-        let divideLocation = parts.indexOf('÷');
-        let evalDivide = eval.divide(parts[divideLocation - 1], parts[divideLocation + 1]);
-        parts.splice(divideLocation - 1, 3, evalDivide);
-    }
-
-    while (parts.includes('+')) {
-        let addLocation = parts.indexOf('+');
-        let evalAdd = eval.add(parts[addLocation - 1], parts[addLocation + 1]);
-        parts.splice(addLocation - 1, 3, evalAdd);
-    }
-
-    while (parts.includes('–')) {
-        let subtractLocation = parts.indexOf('–');
-        let evalSubtract = eval.subtract(parts[subtractLocation - 1], parts[subtractLocation + 1]);
-        parts.splice(subtractLocation - 1, 3, evalSubtract);
-    }
-    if (parts[0] === Infinity) {
-        return `Undefined`;
-    }
-    return parts[0];
-}
-
-//Updates the display
-//The first parameter is a boolean for whether the display should be appended or replaced by the expression
-//The second parameter is the character or string which is being added to the expression
-function updateDisplay(append, char) {
-    if (append) {
-        expression += char;
-        displayValue = expression;
-        input.textContent = displayValue;
-    } else {
-        expression = char;
-        displayValue = expression;
-        input.textContent = displayValue;
+function evaluate(operator, a, b) {
+    switch (operator) {
+        case 'add':
+            return eval.add(a, b);
+        case 'subtract':
+            return eval.subtract(a, b);
+        case 'multiply':
+            return eval.multiply(a, b);
+        case 'divide':
+            return eval.divide(a, b);
     }
 }
 
-let displayValue = '';
+function updateDisplay() {
+    answer = evaluate(currentOperator, lastNum, currentNum);
+    input.textContent = answer;
+    switch (currentOperator) {
+        case 'add':
+            expression.textContent = `${lastNum} + ${currentNum} = `
+            break;
+        case 'subtract':
+            expression.textContent = `${lastNum} - ${currentNum} = `
+            break;
+        case 'multiply':
+            expression.textContent = `${lastNum} x ${currentNum} = `
+            break;
+        case 'divide':
+            expression.textContent = `${lastNum} ÷ ${currentNum} = `
+            break;
+    }
+    currentNum = answer;
+    lastNum = '';
+    currentOperator = '';
+}
+
+let input = document.querySelector('#input');
+let expression = document.querySelector('#expression');
+
+//let inputValue = '0';
+let currentNum = '';
 let answer = '';
-let expression = '';
+let lastNum = '';
+let currentOperator = '';
 
-const input = document.querySelector('#input');
-const lastExpression = document.querySelector('#evaluated-expression');
-
-window.onload = updateDisplay(false, '0');
+input.textContent = '0';
 
 const numbers = Array.from(document.querySelectorAll('.number'));
 for (let number of numbers) {
     number.addEventListener('click', () => {
-        if (expression === answer || expression.toString() === '0' || expression === 'undefined') {
-            updateDisplay(false, number.textContent)
-        } else {
-            updateDisplay(true, number.textContent)
-        }
+        currentNum += number.textContent;
+        input.textContent = currentNum;
     });
 }
 
-const decimal = {
-    decimalButton: document.querySelector('#decimal'),
-    updateDecimal: function () {
-        updateDisplay(true, decimal.decimalButton.textContent);
-        decimal.disable();
-    },
-    enable: function () {
-        this.decimalButton.addEventListener('click', this.updateDecimal);
-    },
-    disable: function () {
-        this.decimalButton.removeEventListener('click', this.updateDecimal);
+const decimal = document.querySelector('#decimal');
+decimal.addEventListener('click', () => {
+    if (!currentNum.includes('.')) {
+        currentNum += decimal.textContent;
+        input.textContent = currentNum;
     }
-};
-
-window.onload = decimal.enable();
+});
 
 const operators = Array.from(document.querySelectorAll('.operator'));
 for (let operator of operators) {
     operator.addEventListener('click', () => {
-        const conditions = ['+', '–', 'x', '÷']
-        if (conditions.some(el => expression.toString().slice(-2).includes(el))) {
-            let newExpression = `${expression.toString().slice(0, -3)} ${operator.textContent} `;
-            updateDisplay(false, newExpression);
-            decimal.disable();
-            decimal.enable();
-        } else {
-            updateDisplay(true, ` ${operator.textContent} `)
-            decimal.disable();
-            decimal.enable();
+        if (currentNum && lastNum) {
+            updateDisplay();
         }
+        currentOperator = operator.id;
+        if (currentNum && !lastNum) lastNum = currentNum;
+
+        expression.textContent = `${lastNum} ${operator.textContent} = `
+        currentNum = '';
     });
 }
 
 const equals = document.querySelector('#equals');
 equals.addEventListener('click', () => {
-    answer = evaluate(expression);
-    lastExpression.textContent = `${expression} =`;
-    updateDisplay(false, answer);
-    if (!answer.toString().includes('.')) {
-        decimal.disable();
-        decimal.enable();
+    if (currentOperator) {
+        updateDisplay();
     }
 });
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
-    answer = '';
-    updateDisplay(false, '0');
-    lastExpression.textContent = '';
-    decimal.disable();
-    decimal.enable();
+    if (clear.textContent === 'AC') {
+        currentNum = '';
+        answer = '';
+        lastNum = '';
+        currentOperator = '';
+        input.textContent = '0';
+        expression.textContent = '';
+    } else if (clear.textContent === 'C') {
+        currentNum = '';
+    }
 });
 
 
